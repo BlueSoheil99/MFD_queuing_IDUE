e192f20386dfe3a9ac78b618f0f9b4a1ab675abc
Ohay-Angah <r01521306@gmail.com>
2022-11-16 14:02:10 -0800

    [update] io

---
+++ inout/utility.py
@@ -0,0 +1,60 @@
+import yaml
+import sumolib.net as sumonet
+import sumolib.xml as sumoxml
+import numpy as np
+
+
+def init_config(fname="config.yaml"):
+    file = yaml.safe_load(open(fname))
+    data_folder = file["data_folder"]
+    network_name = "{}{}".format(data_folder, file["network_name"])
+    feature_name = "{}{}".format(data_folder, file["info_name"])
+    feature = file["feature"]
+    return network_name, feature_name, feature
+
+
+def get_node_pair(net, edge_id):
+    source = net.getEdge(edge_id).getFromNode().getID()
+    sink = net.getEdge(edge_id).getToNode().getID()
+    return source, sink
+
+
+def get_node_index(node_diction, node_id):
+    node_list = list(node_diction.keys())
+    i = node_list.index(node_id)
+    return i
+
+
+def make_adjacency(net, node_diction, edges_diction):
+    n_nodes = len(node_diction.keys())
+    adjacency_mat = np.zeros((n_nodes, n_nodes), dtype=int)
+
+    for key, value in edges_diction.items():
+        source, sink = get_node_pair(net, key)
+        i = get_node_index(node_diction, source)
+        j = get_node_index(node_diction, sink)
+        adjacency_mat[i][j] = 1
+    return adjacency_mat
+
+
+def read_network(net_fname):
+    net = sumonet.readNet(net_fname)
+    nodes = net.getNodes()
+    edges = net.getEdges()
+    return net, nodes, edges
+
+
+def read_node_info(nodes):
+    node_diction = {node.getID(): node.getCoord() for node in nodes}
+    return node_diction
+
+
+def read_edge_info(edges, feature_name, option):
+    edge_diction = {edge.getID(): 0 for edge in edges}
+    edge_stats = sumoxml.parse(feature_name, "edge")
+    for edge in edge_stats:
+        try:
+            edge_diction[edge.id] += float(edge.getAttribute(option))
+        except:
+            print("{} has no attribute: {}".format(edge.id, option))
+    return edge_diction
--- io_handler.py
+++ io_handler.py
@@ -1,17 +1,52 @@
 
 #Hey Pranati!
 
-# I suggest that you make a new directory (name can be io) and make other .py files related to this file.
+# I suggest that you make a new directory (name can be inout) and make other .py files related to this file.
 # Also you can have a .txt file in that folder to save the address of input files. You can hardcode the addresses or
 # input file names for now but we'll have to make it cleaner later
+import numpy as np
+import matplotlib.pyplot as plt
 
-def get_network(input_addresses):
-    # your code here
-    return list_of_densities, adjacency_matrix
+from inout import utility as util
+from inout import plot_network as vis
+
+
+def get_network(input_addresses="config.yaml"):
+    net_fname, info_fname, option = util.init_config(input_addresses)
+    # read network
+    net, nodes, edges = util.read_network(net_fname)
+    # organize the network info into dictionaries
+    node_diction = util.read_node_info(nodes)
+    edge_diction = util.read_edge_info(edges, info_fname, option)
+    # convert the network into adjacency matrix and density list
+    # now I aggregate densities of whole day
+    # TODO: revise the code if we need a specific time frame of density info
+    adjacency_matrix = util.make_adjacency(net, node_diction, edge_diction)
+    list_of_edges = edge_diction.keys()
+    list_of_densities = edge_diction.values()
+    return net, list_of_edges, list_of_densities, adjacency_matrix
     # return matrix with numpy array. That should be more efficient
 
 
-def show_network(list_of_segment):
+def show_network(net, edges_list, region_id, width_edge=2, alpha=0.5, mapscale=1.0, colormap="tab10"):
+    fig, ax = plt.init_plot()
+
+    vmin = min(region_id)
+    vmax = max(region_id)
+
+    colormap = vis.init_colors(colormap, vmin, vmax)
+
+    for key, value in zip(edges_list, region_id):
+        shape = net.getEdge(key).getShape()
+        x_vec = np.array(shape)[:, 0]
+        y_vec = np.array(shape)[:, 1]
+        ax.plot(x_vec * mapscale, y_vec * mapscale, color=vis.get_color(colormap, value, "partition"),
+                lw=width_edge, alpha=alpha, zorder=-100)
+
+    plt.xlabel("x coord")
+    plt.ylabel("y coord")
+    plt.show()
+
     # plot the network with each segment defined with a color
     # the input is list with len=|V| each element shows the seg# for each link
     # ex: we have 4 segments, the list would be like: [1, 1, 1, 2, 2, 3, 4, 4, 3, 1 ,...]
