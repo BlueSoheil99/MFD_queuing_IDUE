61a9425d015bc00ac59fcbe7c4140cbeff1b8828
PranatiAwasthi <pranati.awsthi@gmail.com>
2022-11-21 01:51:27 -0800

    changes to plot network

--- io_handler.py
+++ io_handler.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from inout import plot_network as pln
 from inout import utility as util
 from inout import plot_network as vis
 
@@ -26,7 +26,7 @@ def get_network(input_addresses="config.yaml"):
 
 
 def show_network(net, edges_list, region_id, width_edge=2, alpha=0.5, mapscale=1.0, colormap="tab10"):
-    fig, ax = plt.init_plot()
+    fig, ax = pln.init_plot()
 
     vmin = min(region_id)
     vmax = max(region_id)
@@ -37,7 +37,7 @@ def show_network(net, edges_list, region_id, width_edge=2, alpha=0.5, mapscale=1
         shape = net.getEdge(key).getShape()
         x_vec = np.array(shape)[:, 0]
         y_vec = np.array(shape)[:, 1]
-        ax.plot(x_vec * mapscale, y_vec * mapscale, color=vis.get_color(colormap, value, "partition"),
+        ax.plot(x_vec * mapscale, y_vec * mapscale, color=vis.get_color(colormap, value),
                 lw=width_edge, alpha=alpha, zorder=-100)
 
     plt.xlabel("x coord")
@@ -45,7 +45,22 @@ def show_network(net, edges_list, region_id, width_edge=2, alpha=0.5, mapscale=1
     plt.show()
 
 
-get_network(input_addresses="config.yaml")
+net, list_of_edges, list_of_densities, adjacency_matrix = get_network(input_addresses="config.yaml")
+print(len(list_of_edges))
+regid=[]
+
+## TO DO: Region Id to be updated by Soheil using algorith to visulaize map
+##TO DO: Map plot colors and details- Pranati
+
+#giving each region uniform id (manually for time being to check plot)
+a = int(len(list_of_edges)/2)
+for i in range(a):
+    regid.append(1)
+for i in range(a+1, len(list_of_edges)):
+    regid.append(2)
+print(regid)
+print(len(regid))
+show_network(net=net, edges_list=list_of_edges, region_id=regid)
     # plot the network with each segment defined with a color
     # the input is list with len=|V| each element shows the seg# for each link
     # ex: we have 4 segments, the list would be like: [1, 1, 1, 2, 2, 3, 4, 4, 3, 1 ,...]
