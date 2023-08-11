40ff9c7d5c699cff91627aa6bac3fd600145c8c7
Bluesoheil99 <soheil.k.78@gmail.com>
2023-08-11 10:08:22 -0700

    more updates on point queue model code inputs

--- input_for_pq/demand_mat_gen.py
+++ input_for_pq/demand_mat_gen.py
@@ -2,32 +2,35 @@ import scipy.io
 import numpy as np
 
 
-def generate_demand_mat(net_edges_and_labels:dict, vehroute_xml, out_adr='output/pq_input/demand.mat'):
+def generate_demand_mat(net_edges_and_labels:dict, vehroute_xml, time_intervals=180000, out_adr='output/pq_input/demand.mat'):
     # 1- MAKE EMPTY NUMPY ARRAY
-    # import pdb
-    # pdb.set_trace()
     n_regions = len(np.unique(np.array(list(net_edges_and_labels.values()))))
-    time = 180000  # todo change
-    matrix = np.zeros((n_regions, n_regions, time))
+    matrix = np.zeros((n_regions, n_regions, time_intervals))
 
     # 2- READ TRIPINFO.XML FILE
     for vehicle in vehroute_xml.findall('./vehicle'):
-        dep_step = int(float(vehicle.get('depart')) * 10)-18000  # because simulation step interval is 0.1 seconds
-        # todo using 18000 is not clean
-        for route in vehicle.iter('route'):
-            # route = vehicle.get('route')
-            route_edges = route.get('edges').split()
-            origin, destination = None, None
-            for edge in route_edges[:5]:
-                if net_edges_and_labels.get(edge) is not None:
-                    origin = net_edges_and_labels[edge]
-                    break
-            for edge in reversed(route_edges[-5:]):
-                if net_edges_and_labels.get(edge) is not None:
-                    destination = net_edges_and_labels[edge]
-                    break
-            if origin is not None and destination is not None:
-                matrix[origin, destination, dep_step] += 1
+        if vehicle.get('type') == 'passenger':
+            dep_step = int(float(vehicle.get('depart')) * 10)-18000  # because simulation step interval is 0.1 seconds
+            # todo using 18000 is not clean
+            for route in vehicle.iter('route'):
+                route_edges = route.get('edges').split()
+                origin, destination = None, None
+
+                for edge in route_edges[:5]:
+                    if net_edges_and_labels.get(edge) is not None:
+                        origin = net_edges_and_labels[edge]
+                        break
+
+                for edge in reversed(route_edges[-5:]):
+                    if net_edges_and_labels.get(edge) is not None:
+                        destination = net_edges_and_labels[edge]
+                        break
+
+                if origin is not None and destination is not None:
+                    matrix[origin, destination, dep_step] += 1
+                else:
+                    print(f'vehicle {vehicle.get("id")} not valid origin or destination')
+                    # todo make these okay
 
     # 3- SAVE THE MATRIX IN MATLAB FORMAT
     name = out_adr.split('/')[-1]
--- pq_input_generator.py
+++ pq_input_generator.py
@@ -1,6 +1,7 @@
 import yaml
 import io_handler as io
 import xml.etree.ElementTree as ET
+import pandas as pd
 
 from input_for_pq.demand_mat_gen import generate_demand_mat
 from input_for_pq.region_params_gen import generate_region_params_basic, generate_region_params_mfd
@@ -12,7 +13,7 @@ def _readxml(address):
     return root
 
 
-def _read_addresses(features:list, config_adr='config files/pq_input_generation config.yaml'):
+def _read_yaml(features:list, config_adr='config files/pq_input_generation config.yaml'):
     config_file = yaml.safe_load(open(config_adr))
     addresses = []
     for feature in features:
@@ -26,29 +27,39 @@ if __name__ == '__main__':
     for more information on what this codes outputs should be, look at output/pq_input/readme.md
     
     In this code we work on:
+    config.csv
     demand.mat,
     region_connection.csv,
     region_params_mfd.csv,
     region_params_basic.csv --except limit_n which should be set manually based on MFD investigation
     '''
 
-    # read addresses
-    config_addresses = 'config files/pq_input_generation config.yaml'
-    tripinfo_adr, vehroute_adr, demand_out_adr = \
-        _read_addresses(['trip_info', 'veh_route', 'demand_output_adr'], config_addresses)
+    # read config file and related network and files
+    config_address = 'config files/pq_input_generation config.yaml'
+    tripinfo_adr, vehroute_adr, pq_input_folder = \
+        _read_yaml(['trip_info', 'veh_route', 'pq_input_folder'], config_address)
 
-    # # read files
     print('reading xml files')
     # tripinfo_xml = _readxml(tripinfo_adr)
     vehroute_xml = _readxml(vehroute_adr)
 
-    # # reading the segmented network
     print('reading segmented network')
-    net, edges, densities, adj_mat, labels = io.get_network(config_addresses)
+    net, edges, densities, adj_mat, labels = io.get_network(config_address)
     edges_and_labels = {key: value for key, value in zip(edges, labels)}
 
     # generate inputs for perimeter control/point queue model codes
-    # generate_demand_mat(edges, labels, tripinfo_adr, demand_out_adr)
-    generate_demand_mat(edges_and_labels, vehroute_xml, demand_out_adr)
+    # # config.csv
+    config_csv_keys = ['model_type', 'time_interval (s)', 'simulation_steps', 'deman_stop_step']
+    config_dict = {key: [val] for key, val in zip(config_csv_keys, _read_yaml(config_csv_keys, config_address))}
+    print(config_dict)
+    data = pd.DataFrame(config_dict)
+    data.to_csv(pq_input_folder+'config.csv', index=False)
+
+    # # demand.mat
+    generate_demand_mat(edges_and_labels, vehroute_xml,
+                        time_intervals=int(config_dict['simulation_steps'][0]),
+                        out_adr=pq_input_folder+'demand.mat')
+
+    # #
     generate_region_params_basic(vehroute_xml)
     generate_region_params_mfd()
