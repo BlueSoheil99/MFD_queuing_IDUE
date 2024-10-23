implemented completion rate scaling. adjusted min/max outflow rates (fixed capacity of unsignalized intersections) started working on implementing pseudo regions for our outputs

input_for_pq/draw_plots.py
@@ -48,7 +48,8 @@ def draw_aggregated_data(mat, window_size, n_lines, sim_start, label):
     plt.show()
 
 
-def draw_empirical_completion_rates(vehicle_accumulation, vehicle_completion, window_size, label, with_scatter=False, with_intercept=True):
+def draw_empirical_completion_rates(vehicle_accumulation, vehicle_completion, window_size, label,
+                                    with_scatter=False, with_intercept=True, scale_after_peak=None):
     num_labels = vehicle_accumulation.shape[0]
     completion_lines = dict()
     for i in range(num_labels):
@@ -62,20 +63,43 @@ def draw_empirical_completion_rates(vehicle_accumulation, vehicle_completion, wi
             plt.scatter(N, C, s=2, color=cmap(i))
 
         # fitting a 3rd degree line
-        if with_intercept:
-            polyfit_c = np.polyfit(N, C, 3)
-        else:
-            x = np.array(N)
-            y = np.array(C)
-            polyfit_c = np.zeros(4)
-            X = np.vstack([x ** i for i in range(3, 0, -1)]).T
-            polyfit_c[:3] = np.linalg.lstsq(X, y, rcond=None)[0]
-
-        completion_lines[i] = polyfit_c
-        # x_curve = np.linspace(min(N), max(N) + 500, 1000)
+        polyfit_c = _get_polyfit_deg3(N, C, with_intercept)
         y_curve = np.polyval(polyfit_c, x_curve)
-        # plt.plot(x_curve, y_curve, color=cmap(i), linewidth=0.6)
         plt.plot(x_curve, y_curve, color=cmap(i), label=f'region {i + 1}')
+
+        # scaling the points after peak so that jam n goes higher (scaling N, not C)
+        if scale_after_peak is not None:
+            scale = scale_after_peak.get(i)
+            if scale!= None:
+                # find peak
+                peakx, jamx = _get_max_and_jam_x(polyfit_c)
+                # scale the N's after peak
+                N_new = [scale * i if i > peakx else i for i in N]
+                x_curve = np.linspace(min(N_new), int(max(N_new) * 0.95), 1000)
+                polyfit_c = _get_polyfit_deg3(N_new, C, with_intercept)
+                y_curve = np.polyval(polyfit_c, x_curve)
+                plt.plot(x_curve, y_curve, color=cmap(i), label=f'region {i + 1} scaled after peak', linestyle='--')
+
+                # # scale the N's after 2*peak
+                # N_new = [scale * i if i > 2*peak else i for i in N]
+                # x_curve = np.linspace(min(N_new), int(max(N_new) * 0.95), 1000)
+                # polyfit_c = _get_polyfit_deg3(N_new, C, with_intercept)
+                # y_curve = np.polyval(polyfit_c, x_curve)
+                # plt.plot(x_curve, y_curve, color=cmap(i), label=f'region {i + 1} scaled after 2peak', linestyle='dotted')
+
+                # scale the N's after peak with linear ratio
+                N_new = [i + jamx*(scale-1)*((i-peakx)/(jamx-peakx)) if i > peakx
+                         else i
+                         for i in N]
+                x_curve = np.linspace(min(N_new), int(max(N_new) * 0.95), 1000)
+                polyfit_c = _get_polyfit_deg3(N_new, C, with_intercept)
+                y_curve = np.polyval(polyfit_c, x_curve)
+                plt.plot(x_curve, y_curve, color=cmap(i), label=f'region {i + 1} scaled linearly', linestyle='dotted')
+
+                # plt.scatter(N, C, s=2, color=cmap(i), alpha=0.5)
+                # plt.scatter(N_new, C, s=2, color=cmap(i))
+        completion_lines[i] = polyfit_c
+
     plt.legend()
     plt.title(label)
     plt.xlabel('Number of vehicles')
@@ -101,3 +125,22 @@ def draw_calculated_completion_rates(vehicle_accumulation, window_size, mfd_fit_
     plt.xlabel('Number of vehicles')
     plt.ylabel('veh/second')
     plt.show()
+
+
+def _get_polyfit_deg3(x_values, y_values, with_intercept):
+    if with_intercept:
+        polyfit_c = np.polyfit(x_values, y_values, 3)
+    else:
+        x = np.array(x_values)
+        y = np.array(y_values)
+        polyfit_c = np.zeros(4)
+        X = np.vstack([x ** i for i in range(3, 0, -1)]).T
+        polyfit_c[:3] = np.linalg.lstsq(X, y, rcond=None)[0]
+    return polyfit_c
+
+
+def _get_max_and_jam_x(polyfit):
+    poly_function = np.poly1d(polyfit)
+    poly_derivative = poly_function.deriv()
+    critical_points = np.roots(poly_derivative)
+    return min(critical_points), max(critical_points)

pq_input_generator.py
@@ -116,16 +116,18 @@ if __name__ == '__main__':
     # Generate inputs for perimeter control/point queue model codes
     print('### GENERATING config.csv')
     config_csv_keys = ['model_type', 'time_interval (s)', 'simulation_steps', 'demand_stop_step',
-                       'min_outflow_zero']
+                       'min_outflow_zero', 'pseudo_region_included']
     config_dict = {key: [val] for key, val in zip(config_csv_keys, _read_yaml(config_csv_keys, config_address))}
     print(config_dict)
     data = pd.DataFrame(config_dict)
     data.to_csv(pq_input_folder+'config.csv', index=False)
 
+    include_pseudo_regions = True if config_dict['pseudo_region_included']=='1' else False
+
     print('### GENERATING demand.mat')
     time_interval, sim_start = _read_yaml(['time_interval (s)', 'sim_start'], config_address)
     demand_matrix = generate_demand_mat(edges_and_labels, vehroute_xml, demand_xml,
-                                        increase_percentage=12,
+                                        increase_percentage=15,
                                         time_interval=float(time_interval),
                                         sim_start=int(sim_start),
                                         sim_steps=int(config_dict['demand_stop_step'][0]),
@@ -142,15 +144,16 @@ if __name__ == '__main__':
     draw_plots.show_vehicle_accumulation(vehicle_accumulation, np.unique(labels), int(sim_start))
     draw_plots.draw_aggregated_data(vehicle_completion, window_size, num_labels, int(sim_start),
                                     label=f'completed trips for each region. aggregation window: {window_size}s')
-    draw_plots.draw_empirical_completion_rates(vehicle_accumulation, vehicle_completion, window_size,
-                                               label='calibrated sim. Completion rates')
+    # draw_plots.draw_empirical_completion_rates(vehicle_accumulation, vehicle_completion, window_size,
+    #                                            label='calibrated sim. Completion rates')
 
     # from the congested simulation
     vehicle_accumulation, vehicle_completion = get_vehicle_accumulation_and_completion(routes_and_times_cong, edges_and_labels)
     completion_lines = draw_plots.draw_empirical_completion_rates(vehicle_accumulation, vehicle_completion, window_size,
                                                                   label='Trips exited from/finished in each region \nempirical comp. rate',
                                                                   with_scatter=False,
-                                                                  with_intercept=False)
+                                                                  with_intercept=False,
+                                                                  scale_after_peak={5: 1.2, 6: 1.5})
 
     # Showing trip generation trends
     n_origins, n_destinations, dep_steps = np.shape(demand_matrix)
@@ -192,7 +195,7 @@ if __name__ == '__main__':
     print('### GENERATING region_connection.csv')
     signal_info_adr = _read_yaml(['signal_info_adr'], config_address)[0]
     signal_info = pd.read_csv(signal_info_adr)
-    connection_df = generate_region_connections(net, labels, boundary_ids, signal_info, vehicle_l=4, minGap=1.4, tau=1)
+    connection_df = generate_region_connections(net, labels, boundary_ids, signal_info, vehicle_l=4, minGap=1.5, tau=1)
     # adjusting labels for PQ code use
     connection_df['start_region'] = connection_df['start_region']+1
     connection_df['end_region'] = connection_df['end_region']+1